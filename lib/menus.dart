d85d65e756c0f4cb303a4315acd817ae8a2a0962
gzhang <garyzhangscm@gmail.com>
2021-09-03 09:42:12 -0700

    Change menu display

--- lib/menus.dart
+++ lib/menus.dart
@@ -3,6 +3,7 @@
 import 'package:cwms_mobile/i18n/localization_intl.dart';
 import 'package:cwms_mobile/shared/MyDrawer.dart';
 import 'package:cwms_mobile/shared/bottom_navigation_bar.dart';
+import 'package:cwms_mobile/shared/functions.dart';
 import 'package:cwms_mobile/shared/global.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,7 @@ class _MenusState extends State<Menus> {
 
   @override
   Widget build(BuildContext context) {
-
+/***
     return Scaffold(
         appBar: AppBar(title: Text(
             "CWMS"
@@ -72,7 +73,70 @@ class _MenusState extends State<Menus> {
 
         endDrawer: MyDrawer(),
     );
+    **/
+    return Scaffold(
+
+      appBar: AppBar(title: Text(
+          "CWMS"
+      )),
+      body: Stack(
+        children:  [
+          Container(
+            child: Column(
+              children: <Widget>[
+                    menuItems
+                ],
+              ),
+          )
+        ],
+      ),
+      endDrawer: MyDrawer(),
+    );
   }
+  get menuItems => Expanded(
+    child: Container(
+      padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+      child: GridView.count(
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        crossAxisCount: 2,
+        childAspectRatio: .90,
+        children: List.generate(
+            _menuGroup == null ? 0 : _menuGroup.menuSubGroups.length,
+                (index) {
+          return Card(
+            child: InkWell(
+              onTap: () {
+                _onPressed(index);
+              },
+              child:
+                Center(
+                  child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      // FlutterLogo(),
+                      Image(
+                        image: NetworkImage(
+                            Global.currentServer.url + "/resource/assets/images/mobile/" + _menuGroup.menuSubGroups[index].icon),
+                          //  "http://k8s-staging-zuulserv-707034e5d3-1316291729.us-west-1.elb.amazonaws.com/api/resource/assets/images/mobile/menu_outbound.jpg"),
+                        width: 100.0,
+                      ),
+                      Text(CWMSLocalizations.of(context)
+                          .getMenuDisplayText(
+                          _menuGroup.menuSubGroups[index].i18n,
+                          _menuGroup.menuSubGroups[index].text))],
+                  ),
+                ),
+            ),
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8)
+            ),
+          );
+        }),
+      ),
+    ),
+  );
 
   //模拟异步获取数据
   void _retrieveIcons() async {
