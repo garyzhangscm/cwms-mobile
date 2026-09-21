dced60e002d3eaa91bde7687e7b79e0302ee2544
xiangxiangwang <xiangxiangwang@live.cn>
2026-09-20 21:55:51 -0700

    Restore launcher icon configuration

--- lib/launch_page.dart
+++ lib/launch_page.dart
@@ -109,7 +109,7 @@ class _LaunchPageState extends State<LaunchPage>
               ClipRRect(
                 borderRadius: BorderRadius.circular(28),
                 child: Image.asset(
-                  'assets/icon/claytech_one_icon_v2.png',
+                  'assets/icon/claytech_one_grid.png',
                   width: 112,
                   height: 112,
                 ),
